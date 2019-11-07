712e8eb33f0bdf34c6a3340a624ffeb946c97a6c
Dumitru Lapteacru <lapteacrudumitru@aol.com>
2019-11-07 19:36:03 +0200

    init

---
+++ src/test/java/com/internship/adminpanel/AdminPortalApplicationTests.java
@@ -0,0 +1,12 @@
+package com.internship.adminpanel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminPortalApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+}
