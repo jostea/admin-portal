7ec8ae12645154813317db62ab71cf69f2b594cc
Constantin Castravet <Constantin.Castravet@endava.com>
2020-02-07 10:21:42 +0200

    first_commit

--- src/main/java/TestClass.java
+++
@@ -1,9 +0,0 @@
-public class TestClass {
-    public static void main(String[] args) {
-        String string = "blava vlavalsda                     ";
-
-        string = string.replace("\"", "");
-
-        System.out.println(string);
-    }
-}
